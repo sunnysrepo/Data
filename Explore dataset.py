2c5c96774d442ae18901e01af7327fd719542d8a
SunW <sungit615@gmail.com>
2020-05-17 17:41:22 -0400

    a

--- Explore dataset.py
+++ Explore dataset.py
@@ -6,4 +6,8 @@ from shutil import copyfile
 
 window = tk.Tk()
 window.withdraw()
-file_path = filedialog.askdirectory(title='Select folder where new reports are located')
+# file_path = filedialog.askdirectory(title='Select folder where new reports are located')
+webship_file_path = filedialog.askopenfilename(title="Select WebShip csv file")
+ws_data = pd.read_csv(webship_file_path,encoding = "ISO-8859-1")
+# data = pd.read_excel()
+print(ws_data)
